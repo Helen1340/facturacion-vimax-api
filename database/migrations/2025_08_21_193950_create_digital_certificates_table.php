f0754e0653b09b7e65aaadef3371bbe0ce0bef2c
Jose <josedavidmorenomoreno07@gmail.com>
2025-08-21 17:37:40 -0500

    feature/certificado-digital: Realización completa de certificado digital, se realizaron, el controlador, el modelo, la migracion y el seeders

---
+++ database/migrations/2025_08_21_193950_create_digital_certificates_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('digital_certificates', function (Blueprint $table) {
+
+            // relacion con la tabla company
+            //$table->unsignedBigInteger('company_id');
+            //$table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+
+            $table->id();
+            $table->string('nombre_certificado', 225);
+            $table->text('ruta_certificado');
+            $table->string('numero_serial', 100);
+            $table->string('contrasena', 150);
+            $table->date('fecha_inicio');
+            $table->date('fecha_fin');
+            $table->enum('estado', ['Vigente', 'Vencido', 'Revocado']);
+            $table->string('entidad_emisora', 100);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('digital_certificates');
+    }
+};
