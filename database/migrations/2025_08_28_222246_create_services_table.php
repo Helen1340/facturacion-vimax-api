ab46b6a7322a765fc7651fb1b7fe4fab1b343d5a
Pw-Wilfran <wilfranpilcue@gmail.com>
2025-08-28 18:04:34 -0500

    tabla services migracion, modelo y controlador, llenado con factory

---
+++ database/migrations/2025_08_28_222246_create_services_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            
+
+            $table->unsignedBigInteger('measurementunit_id');
+            // Restricción de clave foránea
+            //$table->foreign('measurementunit_id')->references('id')->on('measurement_units')->onDelete('cascade');
+
+            $table->string('nombre', 150);
+            $table->string('descripcion', 150)->nullable();
+            $table->string('codigo_servicio', 50)->unique();
+            $table->decimal('precio_unitario', 18, 6);
+            $table->enum('estado', ['Activo', 'Inactivo'])->default('Activo');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('services');
+    }
+};
