4e3463d6a1c473d671bda46b19e4057ec5507e36
Pw-Wilfran <wilfranpilcue@gmail.com>
2025-08-28 18:24:18 -0500

    tabla Products migracion, modelo y controlador, llenado con factory

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'measurementunit_id' => $this->faker->numberBetween(1, 10), // Ajusta según los ids válidos
+            'codigo_estandar' => $this->faker->optional()->bothify('EST-####'),
+            'codigo_producto' => $this->faker->unique()->bothify('PRD-####'),
+            'nombre' => $this->faker->words(2, true),
+            'descripcion' => $this->faker->sentence(6),
+            'precio_unitario' => $this->faker->randomFloat(2, 10, 1000),
+            'estado' => $this->faker->randomElement(['Activo', 'Inactivo']),
+        ];
+    }
+}
