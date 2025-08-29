0436d61621a02c13b66902ab8e882365310425fd
Helen <helendiaz.1340@gamil.com>
2025-08-28 20:41:36 -0500

    Completo

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -31,6 +31,7 @@ class DatabaseSeeder extends Seeder
         ElectronicDocumentsSeeder::class,
         TaxTableSeeder::class,
         Users::class,
+        ElectronicInvoiceseeder::class,
         
         
     ]);
